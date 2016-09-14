73df0c4cbe9fc70cf40133333e60582dd0110f99
root <root@localhost.localdomain>
2016-09-14 11:09:30 +0900

    add pdo

---
+++ database/pdo/Connecter.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @file   Connector.php
+ * @brief  connect to RDMS
+ * @author simpart
+ */
+namespace tetraring\database\pdo;
+
+class Connector {
+    private $conn = null;
+    
+    /**
+     * get pdo object
+     */
+    public function __construct($host, $dbnm, $user, $pass) {
+        try {
+            $pdo = new \PDO(
+                'mysql:host=' . $host . ';dbname=' . $dbnm . ';charset=utf8',
+                $user ,
+                $pass ,
+                array(
+                    PDO::ATTR_EMULATE_PREPARES        => false,
+                    PDO::MYSQL_ATTR_READ_DEFAULT_FILE => '/etc/my.cnf'
+                )
+            );
+            if(true === is_null($pdo)) {
+                throw new \Exception('pdo connect failure');
+            }
+            $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->conn = $pdo;
+        } catch (\Exception $e) {
+            DERR_EXCPCNT_METHOD;
+        }
+    }
+    
+    public function select($tbl, $fil) {
+        try {
+            $sql = 'select * from ' . $tbl;
+            if (null !== $fil) {
+                $sql   .= ' where ';
+                $first  = true;
+                foreach ($fil as $fil_key => $fil_val) {
+                    if (true === $first) {
+                        $first = false;
+                    } else {
+                        $sql .= ' and ';
+                    }
+                    $sql .= $fil_key . ' = ' . $fil_val;
+                }
+            }
+            $stmt    = $this->conn->prepare($sql);
+            $stmt->execute();
+            $ret_val = null;
+            while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $ret_val[] = $row;
+            }
+            return $ret_val;
+        } catch (\Exception $e) {
+            DERR_EXCPCNT_METHOD;
+        }
+    }
+}
+/* end of file */
