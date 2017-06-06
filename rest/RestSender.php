582ecfa88d76303cfc99434f6ecee8dd5be63865
root <root@localhost.localdomain>
2017-06-06 13:08:56 +0900

    supported option

--- rest/RestSender.php
+++ rest/RestSender.php
@@ -5,6 +5,7 @@
  * @author simpart
  * @note   MIT license
  */
+namespace ttr\rest;
 
 /**
  * @class RestSender
@@ -21,8 +22,16 @@ class RestSender
             if (null === $uri) {
                 throw new \Exception('invalid parameter');
             }
+            $this->uri = $uri;
         } catch (\Exception $e) {
-
+            throw new \Exception(
+                      PHP_EOL   .
+                      'File:'   . __FILE__         . ',' .
+                      'Line:'   . __line__         . ',' .
+                      'Class:'  . get_class($this) . ',' .
+                      'Method:' . __FUNCTION__     . ',' .
+                      $e->getMessage()
+                  );
         }
     }
     
@@ -51,7 +60,14 @@ class RestSender
 #
 #            return $ret_val;
         } catch (\Exception $e) {
-
+            throw new \Exception(
+                      PHP_EOL   .
+                      'File:'   . __FILE__         . ',' .
+                      'Line:'   . __line__         . ',' .
+                      'Class:'  . get_class($this) . ',' .
+                      'Method:' . __FUNCTION__     . ',' .
+                      $e->getMessage()
+                  );
         }
     }
     
@@ -60,25 +76,43 @@ class RestSender
      *
      * @param $prm : (array) post parameter
      */
-    public function sendPost ($prm)
+    public function sendPost ($prm, $opt=null)
     {
         try {
-            $curl = curl_init($uri);
+            $curl = curl_init($this->uri);
             curl_setopt($curl, CURLOPT_POST, true);
-            // application/x-www-form-urlencoded
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($prm));
+            // application/json
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($prm));
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
             curl_setopt($curl, CURLOPT_MAXREDIRS     , 5);
+            
+            if (null !== $opt) {
+                foreach ($opt as $key => $val) {
+                    curl_setopt($curl, $key, $val);
+                }
+            } 
+            
             $ret_val = curl_exec($curl);
+            if (false === $ret_val) {
+                 var_dump(curl_getinfo($curl));
+                 var_dump(curl_errno($curl));
+            }
             curl_close($curl);
 
             return $ret_val;
         } catch (\Exception $e) {
-
+            throw new \Exception(
+                      PHP_EOL   .
+                      'File:'   . __FILE__         . ',' .
+                      'Line:'   . __line__         . ',' .
+                      'Class:'  . get_class($this) . ',' .
+                      'Method:' . __FUNCTION__     . ',' .
+                      $e->getMessage()
+                  );
         }
     }
 }
