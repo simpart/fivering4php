d1106c3a0097bc336d3ca7e24f3806d69bcf4d38
simpart <simpart>
2018-04-12 16:12:26 +0900

    mod require

--- require.php
+++ require.php
@@ -5,6 +5,6 @@
     require_once(__DIR__ . '/class/require.php');
     require_once(__DIR__ . '/rest/require.php');
     require_once(__DIR__ . '/format/require.php');
-    require_once(__DIR__ . '/database/require.php');
+    require_once(__DIR__ . '/db/require.php');
     require_once(__DIR__ . '/error/require.php');
     require_once(__DIR__ . '/header/require.php');
